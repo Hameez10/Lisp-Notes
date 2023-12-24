05dd442bf1741cbbedd575fe5645239ddef23638
Hameez Iqbal <76129615+Hameez10@users.noreply.github.com>
2023-12-24 14:50:17 -0500

    Update BasicMathOperation.lisp

--- BasicMathOperation.lisp
+++ BasicMathOperation.lisp
@@ -1,2 +1,3 @@
+# Math Operations
 ;;; You may evaluate many kinds of commands on Lisp, but the most simple and most commonly used type is a function application. It is formatted like this (f a1 a2 ... an),
 ;;where 'f' is the function and 'a' is an arguement. 
